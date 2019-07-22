9f138f61f9bc151963b324bff66fb33549671420
NicolasT <nicolastem@hotmail.fr>
2019-07-22 12:00:45 +0200

    switch admin panel to materialize

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,11 +1,4 @@
-require('bootstrap/dist/css/bootstrap.min.css');
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
-const $ = require('jquery');
-require('bootstrap');
+
 require('../css/admin.css');
-
-$(document).ready(function() {
-	$('[data-toggle="popover"]').popover();
-});
-
