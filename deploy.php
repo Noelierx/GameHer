582a062774d9799bd0c0f453ed1469f252c92901
Nicolas T <nicol@DESKTOP-S5MBI3F.(none)>
2019-08-10 15:08:43 +0200

    test deployer

---
+++ deploy.php
@@ -0,0 +1,34 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony4.php';
+
+set('application', 'test');
+set('repository', 'git@github.com:Noelierx/GameHer.git');
+set('allow_anonymous_stats', false);
+set('git_tty', false);
+set('default_timeout', 600);
+
+add('shared_files', []);
+add('shared_dirs', ['public/uploads']);
+add('writable_dirs', ['public/uploads']);
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+task('deploy:npm:install', 'npm install')->desc('Installing node modules');
+task('deploy:npm:build', 'npm run build')->desc('Running webpack');
+
+after('deploy:failed', 'deploy:unlock');
+
+before('deploy:symlink', 'database:migrate');
+before('deploy:cache:clear', 'deploy:npm:install');
+before('deploy:cache:clear', 'deploy:npm:build');
+
+//Setup host
+host('gameher.fr')
+	->user('gameher')
+	->multiplexing(false)
+	->forwardAgent(true)
+	->set('deploy_path', '~/{{application}}');
