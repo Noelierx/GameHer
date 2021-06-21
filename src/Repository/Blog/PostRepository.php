7c0e562f5fd117e515e336621b08d6d16090fb77
Noëlie <noelieroux11@gmail.com>
2021-06-21 15:49:19 +0200

    feat(searchbar): make searchbar work

--- src/Repository/Blog/PostRepository.php
+++ src/Repository/Blog/PostRepository.php
@@ -40,6 +40,11 @@ class PostRepository extends ServiceEntityRepository
         if ($options['author'] instanceof User) {
             $qb->andWhere('p.author = :author')->setParameter('author', $options['author']);
         }
+        if ($options['query'] !== null){
+            $qb
+                ->andWhere('p.title LIKE :query OR p.slug :query OR p.content LIKE :query')
+                ->setParameter('query', '%'.$options['query'].'%');
+        }
 
         return (new Paginator($qb))->paginate($page);
     }
